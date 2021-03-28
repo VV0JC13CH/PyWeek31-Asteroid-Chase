8fb7ef0b0ece984c57241870b545a74f8a89cebe
wojciech <wojciech@bitstudio.dev>
2021-03-28 22:36:44 +0200

    add settings view placeholder

---
+++ src/settings.py
@@ -0,0 +1,24 @@
+"""
+settings.py
+
+Place for SettingsView class.
+"""
+
+import arcade
+from menu import MenuView
+
+
+class SettingsView(arcade.View):
+    def on_show(self):
+        arcade.set_background_color(arcade.color.WHITE)
+
+    def on_draw(self):
+        self.window.fps_counter.on_draw_start()
+        arcade.start_render()
+        arcade.draw_text("Settings Screen", self.window.width/2, self.window.height/2,
+                         arcade.color.BLACK, font_size=50, anchor_x="center")
+        self.window.fps_counter.on_draw_finish()
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        menu_view = MenuView()
+        self.window.show_view(menu_view)
